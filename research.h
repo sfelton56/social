39be6b7a91269cf60963147d4a96f9c85e440fc1
Sloan Felton <sloanfelton@gmail.com>
2017-09-01 15:35:47 -0400

    Create research.h

---
+++ research.h
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+ <meta charset="utf-8">
+  <title>Sloan Felton</title>
+  <meta name="viewport"content="initial-scale=1">
+
+  <meta property="og:title"content="Sloan Felton">
+  <meta property="og:url"content="/">
+      
+  <!-- Icons -->
+  <link rel="apple-touch-icon"href="     ">
+  <link rel="shortcut icon" href="https://sfelton56.github.io/social/favicon.ico" type="image/x-icon">
+
+<head>
+<style>
+.button {
+    background-color: #800000;
+    border: none;
+    color: white;
+    padding: 15px 64px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 20px;
+    position: relative;
+    margin-left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+}
+body {
+	position: absolute;
+    margin-left: 50%;
+    top:50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+    }
+</style>
+</head>
+ 
+  <body style="background-color:#FFD700;">
+	  
+  <p><a href="instagram://user?username=sloan_felton" class="button" >Instagram</a></p>
+  <p><a href="https://www.snapchat.com/add/sfelton56" class="button">Snapchat</a></p>
+  <p><a href="twitter://user?screen_name=sloanotlef" class="button">Twitter</a></p>
+  <p><a href="https://soundcloud.com/3mamba" class="button">SoundCloud</a></p
+
+  </body>
+</html>
